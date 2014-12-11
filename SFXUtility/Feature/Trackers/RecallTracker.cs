0c25b8f5fdc9fb51cf0635fc6297e0f058276d3b
zUsername <nhok.huyvo@gmail.com>
2014-12-11 15:33:20 +0700

    Update Tracker for Shen & TF

--- SFXUtility/Feature/Trackers/RecallTracker.cs
+++ SFXUtility/Feature/Trackers/RecallTracker.cs
@@ -84,7 +84,7 @@ namespace SFXUtility.Feature
 				int count = 0;
 				foreach (Recall recall in _recalls)
 				{
-					if (recall.LastStatus != Class.RecallPacket.RecallStatus.Unknown)
+					if (recall.LastStatus != Packet.S2C.Teleport.Status.Unknown)
 					{
 						var text = recall.ToString();
 						if (recall.Update() && !string.IsNullOrWhiteSpace(text))
@@ -131,32 +131,25 @@ namespace SFXUtility.Feature
 			{
 				if (!Enabled)
 					return;
-
-				if (args.PacketData[0] == Class.RecallPacket.Header)
+				
+				if (args.PacketData[0] == Packet.S2C.Teleport.Header)
 				{
-					var packet = Class.Recall.Decode(args.PacketData);
-					var recall = _recalls.FirstOrDefault(r => r.Hero.NetworkId == packet.UnitNetworkId);
+					Packet.S2C.Teleport.Struct decoded = Packet.S2C.Teleport.Decoded(args.PacketData);
+					var recall = _recalls.FirstOrDefault(r => r.Hero.NetworkId == decoded.UnitNetworkId);
 					if (!Equals(recall, default(Recall)))
 					{
-						recall.Duration = packet.Duration;
-						recall.LastStatus = packet.Status;
+						recall.Duration = decoded.Duration;
+						recall.LastStatus = decoded.Status;
+						recall.LastStatusType = decoded.Type;
 					}
-					if (packet.Status == Class.RecallPacket.RecallStatus.RecallFinished)
-					{
-						IoC.Resolve<Mediator>().NotifyColleagues(Name + "_Finished", packet.UnitNetworkId);
-					}
-					if (packet.Status == Class.RecallPacket.RecallStatus.RecallStarted)
-					{
-						IoC.Resolve<Mediator>().NotifyColleagues(Name + "_Started", packet.UnitNetworkId);
-					}
-					if (packet.Status == Class.RecallPacket.RecallStatus.RecallAborted)
-					{
-						IoC.Resolve<Mediator>().NotifyColleagues(Name + "_Aborted", packet.UnitNetworkId);
-					}
-					if (packet.Status == Class.RecallPacket.RecallStatus.Unknown)
-					{
-						IoC.Resolve<Mediator>().NotifyColleagues(Name + "_Unknown", packet.UnitNetworkId);
-					}
+					if (decoded.Status == Packet.S2C.Teleport.Status.Finish)
+						IoC.Resolve<Mediator>().NotifyColleagues(Name + "_Finished", decoded.UnitNetworkId);
+					if (decoded.Status == Packet.S2C.Teleport.Status.Start)
+						IoC.Resolve<Mediator>().NotifyColleagues(Name + "_Started", decoded.UnitNetworkId);
+					if (decoded.Status == Packet.S2C.Teleport.Status.Abort)
+						IoC.Resolve<Mediator>().NotifyColleagues(Name + "_Aborted", decoded.UnitNetworkId);
+					if (decoded.Status == Packet.S2C.Teleport.Status.Unknown)
+						IoC.Resolve<Mediator>().NotifyColleagues(Name + "_Unknown", decoded.UnitNetworkId);
 				}
 			}
 			catch (Exception ex)
@@ -186,7 +179,7 @@ namespace SFXUtility.Feature
 					_trackers.Menu.AddSubMenu(Menu);
 
 					foreach (
-						Obj_AI_Hero hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsValid && hero.IsEnemy)
+						Obj_AI_Hero hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsValid)
 					)
 					{
 						_recalls.Add(new Recall(hero));
@@ -217,7 +210,8 @@ namespace SFXUtility.Feature
 			public readonly Obj_AI_Hero Hero;
 			private float _duration;
 			private float _lastActionTime;
-			private Class.RecallPacket.RecallStatus _lastStatus;
+			private Packet.S2C.Teleport.Status _lastStatus;
+			private Packet.S2C.Teleport.Type _lastStatusType;
 			private float _recallStart;
 
 			#endregion
@@ -227,12 +221,18 @@ namespace SFXUtility.Feature
 			public Recall(Obj_AI_Hero hero)
 			{
 				Hero = hero;
-				LastStatus = Class.RecallPacket.RecallStatus.Unknown;
+				LastStatus = Packet.S2C.Teleport.Status.Unknown;
+				LastStatusType = Packet.S2C.Teleport.Type.Unknown;
 			}
 
 			#endregion
 
 			#region Properties
+			public Packet.S2C.Teleport.Type LastStatusType
+			{
+				get {return _lastStatusType;}
+				set {_lastStatusType = value;}
+			}
 
 			public float Duration
 			{
@@ -240,14 +240,13 @@ namespace SFXUtility.Feature
 				set { _duration = value/1000; }
 			}
 
-			public Class.RecallPacket.RecallStatus LastStatus
+			public Packet.S2C.Teleport.Status LastStatus
 			{
 				get { return _lastStatus; }
 				set
 				{
 					_lastStatus = value;
-					_recallStart = _lastStatus == Class.RecallPacket.RecallStatus.RecallStarted ||
-						_lastStatus == Class.RecallPacket.RecallStatus.TeleportStart
+					_recallStart = _lastStatus == Packet.S2C.Teleport.Status.Start
 						? Game.Time
 						: 0f;
 					_lastActionTime = Game.Time;
@@ -260,48 +259,61 @@ namespace SFXUtility.Feature
 
 			public override string ToString()
 			{
-				var time = _recallStart + Duration - Game.Time;				
+				var time = _recallStart + Duration - Game.Time;
 				if (time <= 0)
 				{
 					time = Game.Time - _lastActionTime;
 				}
-				if (LastStatus == Class.RecallPacket.RecallStatus.RecallStarted)
-					return string.Format("Recall: {0}({1}%) Recalling ({2:0.00})", Hero.ChampionName,
-					                     (int) Hero.HealthPercentage(), time);
-				if (LastStatus == Class.RecallPacket.RecallStatus.RecallFinished)
-					return string.Format("Recall: {0}({1}%) Recalled ({2:0.00})", Hero.ChampionName,
-					                     (int) Hero.HealthPercentage(), time);
-				if (LastStatus == Class.RecallPacket.RecallStatus.RecallAborted)
-					return string.Format("Recall: {0}({1}%) Aborted", Hero.ChampionName, (int) Hero.HealthPercentage());
-				if (LastStatus == Class.RecallPacket.RecallStatus.TeleportStart)
-					return string.Format("Teleport: {0}({1}%) Teleporting ({2:0.00})", Hero.ChampionName,
-					                     (int) Hero.HealthPercentage(), time);
-				if (LastStatus == Class.RecallPacket.RecallStatus.TeleportEnd)
-					return string.Format("Teleport: {0}({1}%) Ported ({2:0.00})", Hero.ChampionName,
-					                     (int) Hero.HealthPercentage(), time);
-				if (LastStatus == Class.RecallPacket.RecallStatus.TeleportAbort)
-					return string.Format("Teleport: {0}({1}%) Aborted", Hero.ChampionName, (int) Hero.HealthPercentage());
+				if (LastStatusType == Packet.S2C.Teleport.Type.Recall)
+				{
+					if (LastStatus == Packet.S2C.Teleport.Status.Start)
+						return string.Format("Recall: {0}({1}%) Recalling ({2:0.00})", Hero.ChampionName,
+						                     (int) Hero.HealthPercentage(), time);
+					if (LastStatus == Packet.S2C.Teleport.Status.Finish)
+						return string.Format("Recall: {0}({1}%) Recalled ({2:0.00})", Hero.ChampionName,
+						                     (int) Hero.HealthPercentage(), time);
+					if (LastStatus == Packet.S2C.Teleport.Status.Abort)
+						return string.Format("Recall: {0}({1}%) Aborted", Hero.ChampionName, (int) Hero.HealthPercentage());
+				}
+				if (LastStatusType == Packet.S2C.Teleport.Type.Teleport)
+				{
+					if (LastStatus == Packet.S2C.Teleport.Status.Start)
+						return string.Format("Teleport: {0}({1}%) Teleporting ({2:0.00})", Hero.ChampionName,
+						                     (int) Hero.HealthPercentage(), time);
+					if (LastStatus == Packet.S2C.Teleport.Status.Finish)
+						return string.Format("Teleport: {0}({1}%) Ported ({2:0.00})", Hero.ChampionName,
+						                     (int) Hero.HealthPercentage(), time);
+					if (LastStatus == Packet.S2C.Teleport.Status.Abort)
+						return string.Format("Teleport: {0}({1}%) Aborted", Hero.ChampionName, (int) Hero.HealthPercentage());
+				}
+				if (LastStatusType == Packet.S2C.Teleport.Type.Shen || LastStatusType == Packet.S2C.Teleport.Type.TwistedFate)
+				{
+					if (LastStatus == Packet.S2C.Teleport.Status.Start)
+						return string.Format("(R): {0}({1}%) Transporting ({2:0.00})", Hero.ChampionName,
+						                     (int) Hero.HealthPercentage(), time);
+					if (LastStatus == Packet.S2C.Teleport.Status.Finish)
+						return string.Format("(R): {0}({1}%) Ported ({2:0.00})", Hero.ChampionName,
+						                     (int) Hero.HealthPercentage(), time);
+					if (LastStatus == Packet.S2C.Teleport.Status.Abort)
+						return string.Format("(R): {0}({1}%) Aborted", Hero.ChampionName, (int) Hero.HealthPercentage());
+				}
 				return string.Empty;
 			}
 
 			public Color ToColor()
 			{
-				if (LastStatus == Class.RecallPacket.RecallStatus.RecallStarted ||
-				    LastStatus == Class.RecallPacket.RecallStatus.TeleportStart)
+				if (LastStatus == Packet.S2C.Teleport.Status.Start)
 					return Color.Beige;
-				if (LastStatus == Class.RecallPacket.RecallStatus.RecallFinished ||
-				    LastStatus == Class.RecallPacket.RecallStatus.TeleportEnd)
+				if (LastStatus == Packet.S2C.Teleport.Status.Finish)
 					return Color.GreenYellow;
-				if (LastStatus == Class.RecallPacket.RecallStatus.RecallAborted ||
-				    LastStatus == Class.RecallPacket.RecallStatus.TeleportAbort)
+				if (LastStatus == Packet.S2C.Teleport.Status.Abort)
 					return Color.Red;
 				return Color.Black;
 			}
 
 			public bool Update()
 			{
-				var additional = LastStatus == Class.RecallPacket.RecallStatus.RecallStarted ||
-					LastStatus == Class.RecallPacket.RecallStatus.TeleportStart
+				var additional = LastStatus == Packet.S2C.Teleport.Status.Start
 					? Duration + 20f
 					: 20f;
 				if (_lastActionTime + additional <= Game.Time)
